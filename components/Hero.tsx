8267c33225ad20ca889b496c9417d60349e2b88c
ashwinbvs <ashwin@sustally.com>
2024-08-10 12:16:33 +0530

    pointer events removed for the sparkles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,8 +6,8 @@ import { SparklesCore } from "@/components/ui/sparkles";
 const Hero = () => {
   return (
     <>
-    <div className='flex flex-col justify-center items-center mx-auto gap-6 min-h-screen px-4 md:px-24 text-center '>
-    <div className="w-full absolute inset-0 h-screen">
+    <div className='flex flex-col justify-center items-center mx-auto gap-6 min-h-screen px-4 md:px-24 text-center -z-50'>
+    <div className="w-full absolute inset-0 min-h-screen pointer-events-none">
       <SparklesCore
         id="tsparticlesfullpage"
         background="transparent"
@@ -18,9 +18,9 @@ const Hero = () => {
         particleColor="#efde74"
       />
       </div>
-        <h1 className='text-5xl md:text-7xl font-bold'>Bringing the Outdoors to You</h1>
-        <p className='font-mono  text-xl max-w-3xl text-secondary'>Discover our premier camp rental options nestled in breathtaking landscapes. Book your earthly escape today.</p>
-        <Button variant={"destructive"} >Contact Us</Button>
+      <h1 className='text-5xl md:text-7xl font-bold'>Bringing the Outdoors to You</h1>
+      <p className='font-mono  text-xl max-w-3xl text-secondary'>Discover our premier camp rental options nestled in breathtaking landscapes. Book your earthly escape today.</p>
+      <Button variant={"destructive"} className='z-50'>Contact Us</Button>
     </div>
     </>
   )
